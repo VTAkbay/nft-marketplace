7a52c7d64e7816670ddd0acbff01b66445b52af0
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-19 22:20:49 +0300

    feat: create initial Marketplace contract

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+pragma solidity ^0.8.0;
+
+contract Marketplace {
+    IERC20 public xToken;
+
+    constructor(address xTokenAddress) {
+        xToken = IERC20(xTokenAddress);
+    }
+
+    event Market(
+        address tokenAddress,
+        uint256 tokenId,
+        address seller,
+        uint256 price
+    );
+
+    event CancelMarket(address tokenAddress, uint256 tokenId, address seller);
+
+    event Trade(
+        address tokenAddress,
+        uint256 tokenId,
+        address seller,
+        uint256 price
+    );
+
+    mapping(uint256 => uint256) public listingIndices;
+
+    struct Listing {
+        uint256 id;
+        address tokenAddress;
+        uint256 tokenId;
+        address seller;
+        uint256 price;
+    }
+    uint256 private nextListingId;
+
+    Listing[] public listings;
+
+    function market(
+        address tokenAddress,
+        uint256 tokenId,
+        uint256 price
+    ) public {
+        require(price != 0, "Cannot sell for nothing");
+        uint256 listingId = nextListingId++;
+        Listing memory listing = Listing(
+            listingId,
+            tokenAddress,
+            tokenId,
+            msg.sender,
+            price
+        );
+        listings.push(listing);
+        listingIndices[listingId] = listings.length - 1;
+        emit Market(tokenAddress, tokenId, msg.sender, price);
+    }
+
+    function cancelMarket(uint256 listingId) public {
+        uint256 index = listingIndices[listingId];
+        Listing storage listing = listings[index];
+
+        require(listing.seller == msg.sender, "Only seller can cancel");
+
+        Listing storage lastListing = listings[listings.length - 1];
+        listingIndices[lastListing.id] = index;
+        listings[index] = lastListing;
+        listings.pop();
+        delete listingIndices[listingId];
+
+        emit CancelMarket(listing.tokenAddress, listing.tokenId, msg.sender);
+    }
+
+    function buy(uint256 listingId) public {
+        uint256 index = listingIndices[listingId];
+        Listing storage listing = listings[index];
+        require(
+            xToken.transferFrom(msg.sender, listing.seller, listing.price),
+            "Could not send X tokens"
+        );
+
+        IERC721 token = IERC721(listing.tokenAddress);
+        token.safeTransferFrom(msg.sender, listing.seller, listing.tokenId);
+
+        Listing storage lastListing = listings[listings.length - 1];
+        listingIndices[lastListing.id] = index;
+        listings[index] = lastListing;
+        listings.pop();
+        delete listingIndices[listingId];
+
+        emit Trade(
+            listing.tokenAddress,
+            listing.tokenId,
+            listing.seller,
+            listing.price
+        );
+    }
+
+    function getListingById(uint256 listingId)
+        public
+        view
+        returns (Listing memory)
+    {
+        uint256 listing = listingIndices[listingId];
+        return listings[listing];
+    }
+}
