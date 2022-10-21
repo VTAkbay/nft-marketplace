89d4aca332d978959aa1e24c2bab3b79b93b28bc
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-21 23:05:34 +0300

    feat: create debugFaucet function for xtoken.sol

--- contracts/XToken.sol
+++ contracts/XToken.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract XToken is ERC20 {
     constructor() ERC20("XToken", "X") {
         _mint(msg.sender, 1000 * 10**18);
+    }
+
+    function debugFaucet() public {
+        _mint(msg.sender, 10 * 10**18);
     }
 }
