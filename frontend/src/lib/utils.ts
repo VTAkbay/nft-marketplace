8555fc1a67b0507d3aa0c4c255ee8b2155459969
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-24 23:01:55 +0300

    feat: move interfaces to util & create isZeroAddress util

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,8 +1,52 @@
+import { BigNumber } from "ethers";
+
 export { default as marketplaceAbi } from "../abi/contracts/Marketplace.sol/Marketplace.json";
 export { default as xTokenAbi } from "../abi/contracts/XToken.sol/XToken.json";
-export { default as xNftAbi } from "../abi/contracts/XNft.sol/XNft.json";
 
 export const headerPages = [{ key: 0, title: "My Nfts", route: "my-nfts" }];
 
 export const isDev =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+export function isZeroAddress(address: string | undefined) {
+  if (!address) {
+    return null;
+  }
+  if (address === "0x0000000000000000000000000000000000000000") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export declare module interfaces {
+  export interface Listing {
+    id: number;
+    tokenAddress: string;
+    tokenId: number;
+    seller: string;
+    price: BigNumber;
+    tokenURI: string;
+  }
+  export interface Market {
+    listings: Listing[];
+    total: number;
+  }
+
+  export interface ListNftSubmitForm {
+    price: string;
+  }
+
+  export interface Nft {
+    tokenURI: string;
+    tokenAddress: string;
+    tokenId: number;
+    approvedBy: string;
+    listed: boolean;
+  }
+
+  export interface Nfts {
+    nfts: Nft[];
+    total: number;
+  }
+}
